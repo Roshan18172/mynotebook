bdef6a3df7e47f47dc41c9a4021fa2c9eed907cd
Roshan Pal <roshanpal18172@gmail.com>
2025-08-13 23:00:42 +0530

    adding login logic
    addling login logic for authentication using user name and password by comparing password with validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,8 +6,7 @@ const User = require('../models/User'); // Import the User model
 const { body , validationResult } = require('express-validator');
 
 const JWT_SECRET="mysecretkey"; // Secret key for JWT
-
-//create a new user using POST method "/api/auth/createuser"
+//route 1 : create a new user using POST method "/api/auth/createuser"
 router.post('/createuser', [
     body('email', 'Enter a valid email').isEmail(),
     body('name', 'Name must be at least 3 characters').isLength({ min: 3 }),
@@ -23,12 +22,10 @@ router.post('/createuser', [
     if (user) {
         return res.status(400).json({ error: "Sorry, a user with this email already exists" });
     } 
-
     // Hash the password before saving it to the database
     const salt = await bcrypt.genSalt(10);
     const secPassword = await bcrypt.hash(req.body.password, salt);
-   
-
+    // Create a new user
     user = await User.create({
         name: req.body.name,
         email: req.body.email,
@@ -49,6 +46,43 @@ router.post('/createuser', [
         res.status(500).send('Server Error');
     });     
 });
+
+//route 2 : Authenticate a user using POST method "/api/auth/login"
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists()
+], async (req, res) => {
+    // Find the validation errors in this request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+        // Check if user exists
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
+        }
+        // Compare the password with the hashed password
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
+        }
+        // Create a JWT token
+        const data = {
+            user: {
+                id: user.id
+            }
+        };
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+});
+
 
 module.exports = router;
 // This file defines the authentication routes for the application.
