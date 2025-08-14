c47c147e2644bbf61f693dbc434d49fa296db00f
Roshan Pal <roshanpal18172@gmail.com>
2025-08-14 12:29:30 +0530

    adding navbar , about ,  home , footer
    adding a nav bar and using react- router- dom link the path of home ,about after that adding footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
+import React from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to My Notebook</h1>
-      <p>This is a simple notebook application.</p>
-      <button onClick={() => alert('Button clicked!')}>Click Me</button>
-      <footer>
-        <p>&copy; 2023 My Notebook</p>
-      </footer>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" exact element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </Router>
+      <Footer mode="primary" />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h2>Welcome to My Notebook</h2>
+        <p>This is a simple notebook application.</p>
+      
+    </div>
+  )
+}
+
+export default Home
